8871b30ace1aff8b5fdaaa703794ebed90361b44
githubaction009 <140196313+githubaction009@users.noreply.github.com>
2023-11-19 00:00:14 +0600

    feat : db

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,6 +21,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'account_type' => 'individual',
+            'balance' => fake()->numberBetween(1000, 5000),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedDouble('balance', 10, 2)->default(0);
+            $table->enum('account_type', ['individual', 'business']);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
